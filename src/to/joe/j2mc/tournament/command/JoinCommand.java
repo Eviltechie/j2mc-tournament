3f7a49eea4de33b1a1b45c4a5a14bbac953b5f17
Ivan Panarusky <eviltechie@joe.to>
2012-06-19 20:56:24 -0400

    fixed join command

--- src/to/joe/j2mc/tournament/command/JoinCommand.java
+++ src/to/joe/j2mc/tournament/command/JoinCommand.java
@@ -19,13 +19,13 @@ public class JoinCommand extends MasterCommand {
 
 	@Override
 	public void exec(CommandSender sender, String commandName, String[] args, Player player, boolean isPlayer) {
-		if (this.plugin.registrationOpen) {
+		if (this.plugin.participants.contains(player)) {
+			sender.sendMessage(ChatColor.AQUA + "You are already signed up for this tournament");
+		} else if (this.plugin.registrationOpen) {
 			this.plugin.participants.add(player);
 			sender.sendMessage(ChatColor.AQUA + "You have been entered into the tournament");
 			J2MC_Manager.getCore().adminAndLog(ChatColor.RED + sender.getName() + ChatColor.AQUA + " has entered the tournament!");
 			J2MC_Manager.getCore().messageNonAdmin(ChatColor.RED + sender.getName() + ChatColor.AQUA + " has entered the tournament!");
-		} else if (this.plugin.participants.contains(player)) {
-			sender.sendMessage(ChatColor.AQUA + "You are already signed up for this tournament");
 		} else {
 			sender.sendMessage(ChatColor.AQUA + "Tournament registration is closed");
 		}
